969b8ae977ffdf789d8793ee20d31669fccb3a6a
Kunal Mod <devkunalmod@gmail.com>
2022-11-04 11:23:08 +0530

    script for terminal split added

---
+++ 4split.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+xdotool key Super+Up 
+cd ~/Code/ros/learning_ws 
+source ~/scripts/rospath.zsh  
+
+xdotool key ctrl+shift+o 
+sleep 0.1
+xdotool key ctrl+shift+e
+sleep 0.1
+xdotool key alt+Up
+xdotool key ctrl+shift+e
+xdotool key alt+Left
---
+++ rospath.zsh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+source /opt/ros/noetic/setup.zsh
+source ~/Code/ros/learning_ws/devel/setup.zsh
+clear
