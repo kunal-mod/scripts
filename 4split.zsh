c028dc562fa7d151d9f209110eedbbee906b760c
Kunal Mod <devkunalmod@gmail.com>
2023-02-27 14:29:33 +0530

    files updated

--- 4split.zsh
+++ 4split.zsh
@@ -5,9 +5,9 @@ cd ~/Code/ros/learning_ws
 source ~/scripts/rospath.zsh  
 
 xdotool key ctrl+shift+o 
-sleep 0.1
+sleep 0.2
 xdotool key ctrl+shift+e
-sleep 0.1
+sleep 0.2
 xdotool key alt+Up
 xdotool key ctrl+shift+e
 xdotool key alt+Left
